d96d6178d65901877081baffac5ded5865dedecd
神樹桜乃 <kodama@sakuno.moe>
2020-10-12 01:21:37 +0800

    Add swap & pool pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PoolComponent } from './pages/pool/pool.component';
+import { SwapComponent } from './pages/swap/swap.component';
+
+const routes: Routes = [
+  { path: 'swap', component: SwapComponent },
+  { path: 'pool', component: PoolComponent },
+
+  { path: '', redirectTo: '/swap', pathMatch: 'full' },
+  { path: '**', component: SwapComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SwapComponent } from './pages/swap/swap.component';
+import { PoolComponent } from './pages/pool/pool.component';
+import { NavTabsComponent } from './shared/nav-tabs/nav-tabs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwapComponent,
+    PoolComponent,
+    NavTabsComponent
   ],
   imports: [
     BrowserModule,
