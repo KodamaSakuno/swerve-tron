ce0da93e960e3ff45951b852daa2ab582c435ae9
神樹桜乃 <kodama@sakuno.moe>
2020-10-14 02:06:17 +0800

    Fix an error when a bignumber of number type

--- src/app/bignumber.pipe.ts
+++ src/app/bignumber.pipe.ts
@@ -6,11 +6,11 @@ import { BigNumber } from 'bignumber.js';
 })
 export class BigNumberPipe implements PipeTransform {
 
-  transform(value: string | BigNumber | null, decimals = 18): string {
+  transform(value: BigNumber.Value | null, decimals = 18): string {
     if (!value)
       return '';
 
-    if (typeof value === 'string')
+    if (typeof value === 'string' || typeof value === 'number')
       value = new BigNumber(value);
 
     return value.div(new BigNumber(10).pow(decimals)).toString();
