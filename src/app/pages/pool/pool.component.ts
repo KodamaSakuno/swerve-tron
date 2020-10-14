773722d42cac8caba55b059066d434d5880fe449
神樹桜乃 <kodama@sakuno.moe>
2020-10-15 02:55:06 +0800

    Update pool page

--- src/app/pages/pool/pool.component.ts
+++ src/app/pages/pool/pool.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { StateService } from '../../services/state.service';
+import { TokenInfo } from '../../types/TokenInfo';
+import { Token } from '../../constants/tokens';
 
 @Component({
   selector: 'app-pool',
@@ -7,9 +13,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PoolComponent implements OnInit {
 
-  constructor() { }
+  swUSD$: Observable<TokenInfo>;
+
+  constructor(private stateService: StateService) {
+    this.swUSD$ = stateService.getToken$(Token.swUSD);
+  }
 
   ngOnInit(): void {
+    this.stateService.getInitialized$().subscribe(() => {
+      this.stateService.requestTRC20TokenBalance(Token.swUSD);
+    });
   }
 
 }
